644356550eda105bfe98a6186eacd559a679cc74
Rachel <rachel.loziuk@gmail.com>
2016-01-16 18:34:28 -0600

    stlye for alternative welcome header

---
+++ resources/views/welcome-01.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+
+            /*background-color: #c2dde5; light blue*/
+            /*background-color: #bcc8db;  light purple*/
+            /*background-color: #e76630;  orange*/
+            /*background-color: #3b54ac;  blue*/
+
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .header {
+                padding-left: 15px;
+
+            }
+
+            .title {
+                margin-top: 30px;
+                font-size: 60px;
+/*                display: table-cell;
+                vertical-align: middle;*/
+            }
+
+            .sub-title {
+                font-size: 18px;
+                width: 90px;
+
+            }
+
+
+            .left {
+                width: 90px;
+                background-color: #BDCCDD; /* light blue-purple */
+            }
+
+            .right {
+                margin-left: 80px;
+                margin-top: -72px;
+                width: 90px;
+                height: 90px;
+                background-color: #e76630;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+
+
+
+        </style>
+    </head>
+    <body>
+        <div class="header">
+            <div class="title left">Rachel Loziuk</div>
+            <div class="sub-title">Web Developer</div>
+
+            <div class="right"></div>
+        </div>
+
+        <div class="container">
+            <div class="content">
+            </div>
+        </div>
+    </body>
+</html>
