1270bcd4a129913ca63d0da76d996e21cb9123f8
Rachel <rachel.loziuk@gmail.com>
2016-01-20 11:05:19 -0600

    move header to master layout and incorporate Bootstrap for styling

---
+++ resources/views/master.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">
+    </script>
+
+    <title>Laravel</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+    <style>
+
+        /*background-color: #c2dde5; light blue*/
+        /*background-color: #bcc8db;  light purple*/
+        /*background-color: #BDCCDD;  light blue-purple */
+
+        /*background-color: #e76630;  orange*/
+        /*background-color: #3b54ac;  blue*/
+        /*background-color: #EBEBEB;  light gray*/
+
+
+   /*     html, body {
+            height: 100%;
+        }*/
+
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+
+
+        .navbar{
+            background-color: #BDCCDD; /* light blue-purple */
+        }
+
+        .my-name {
+            font-size: 25px;
+            font-weight: bold;
+            text-align: left;
+        }
+
+        .menu.pinata {
+            width: 22px;
+        }
+
+        .banner.pinata {
+            padding-top: 15px;
+        }
+
+        .content {
+            text-align: center;
+            /*display: inline-block;*/
+        }
+
+        .sub-title {
+            font-size: 72px;
+        }
+
+
+
+
+    </style>
+
+</head>
+
+
+
+<body>
+    <!-- NAVIGATION BAR -->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <!-- MENU TOGGLE BUTTON -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <img class="menu pinata" src="images/animal-blue.png">
+                <!--<span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span> -->
+                </button>
+                <!-- PERSONAL NAME/BRAND -->
+                <a class="navbar-brand my-name" href="#">Rachel Loziuk</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+    <!-- MAIN CONTENT -->
+    <div class="container">
+
+        @yield('content')
+
+    </div>
+
+</body>
+
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,71 +1,21 @@
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('master')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-
-            /*background-color: #c2dde5; light blue*/
-            /*background-color: #bcc8db;  light purple*/
-            /*background-color: #e76630;  orange*/
-            /*background-color: #3b54ac;  blue*/
-
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                /*vertical-align: middle;*/
-            }
-
-            .header {
-                background-color: #BDCCDD; /* light blue-purple */
-                height: 150px;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-
-            .sub-title {
-                font-size: 40px;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+@section('title')
+Portfolio
+@stop
 
 
-
-
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="header"></div>
-
-            <div class="content">
-                <div class="title">Rachel Loziuk</div>
+@section('content')
+            <div class="row content">
                 <div class="sub-title">Web Developer</div>
+                <div>
+                    <img class="banner pinata" src="images/animal-skyblue.png">
+                </div>
             </div>
-        </div>
-    </body>
-</html>
+@stop
+
+
 
 
 
