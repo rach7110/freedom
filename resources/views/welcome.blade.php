f209caf93d23887e24a4769c1cb42d2245931d5e
Rachel <rachel.loziuk@gmail.com>
2016-04-02 14:44:54 -0500

    edit welcome page text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@ Rachel's Portfolio
             <div class="row" id="summary">
             	<h2 class="medium-font">Hello there. I'm Rachel Loziuk</h2>
             	<h3 class="font-1">I'm a full stack web developer in Austin, TX</h4>
-            	<p>I write code for the web using HTML, CSS, php, Laravel (a framework for php), JavaScript, and JQuery. I develop projects on Linux, Mac, and Windows operating systems. I'm continuously improving my developer and design skills. I'm active in the Austin tech community, and you can often find me at Women Who Code during the evenings.</p>
+            	<p>I write code for the web using HTML, CSS, PHP, Laravel (a framework for PHP), JavaScript, and JQuery. I develop projects on Mac, Windows, and Linux operating systems. I'm continuously improving my developer and design skills. I'm active in the Austin tech community, and you can often find me at Women Who Code during the evenings.</p>
             </div>
 @stop
 
