a240d3470040e04bcc957ea9c65af65204c77efc
Rachel <rachel.loziuk@gmail.com>
2016-04-02 15:37:40 -0500

    add profile picture to contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,7 +5,31 @@ Contact Me
 @stop
 
 @section('content')
- 
-<h1 class="font-1"> Here's an About Me page. Maybe I'll add an email form too.</h1>
 
+<h1 class="font-1"> Contact</h1>
+<hr>
+
+<div class="row">
+    <!--CONTACT FORM-->
+    <div class="col-md-4 col-md-push-8">
+        <div class="template">
+        Form
+        </div>
+    </div>
+    <!-- ABOUT ME -->
+    <div class="col-md-4 ">
+        <div class="template">
+        About Me
+        </div>
+    </div>
+    <!-- PROFILE PICTURE -->
+    <div class="col-md-4 col-md-pull-8">
+        <div class="template">
+        <img class="profile-picture" src="images/rachel-profile.jpg">
+        </div>
+    </div>
+</div>
+ 
 @stop
+
+
