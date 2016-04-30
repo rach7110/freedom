1bd38121d640c3442ce4cb5276823ffe236d5427
Rachel <rachel.loziuk@gmail.com>
2016-04-30 14:07:17 -0500

    layout for projects page

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -6,6 +6,57 @@ Projects
 
 @section('content')
  
-<h1 class="font-1"> Check out my work!</h1>
+ <!-- HEADER -->
+<div class="row">
+    <div class="col-md-12 font-1">
+        <h1>Check out my work!  </h1>
+        <h3>(we'll get there...)</h3>
+    </div>
+</div>
+
+<!-- BODY -->
+<div id="projects">
+    <div class="row">
+        <!-- PROJECT IMAGE -->
+        <div class="col-md-4 center-inline">
+            <img src="images/animal-skyblue.png">
+        </div>
+        <!-- PROJECT DESCRIPTION -->
+        <div class="col-md-8">
+        <h4>Project Title</h4>
+        <p>
+        Cappuccino, java black trifecta caramelization latte cream in crema percolator. Id to go sweet, froth macchiato, arabica cortado milk whipped trifecta. Filter single origin, americano milk instant affogato crema wings. Bar id black single origin pumpkin spice sit mazagran flavour. Froth, eu milk coffee kopi-luwak fair trade skinny flavour.
+        </p>
+        </div>
+    </div>
+
+        <div class="row">
+        <!-- PROJECT IMAGE -->
+        <div class="col-md-4 center-inline">
+            <img src="images/animal-orange.png">
+        </div>
+        <!-- PROJECT DESCRIPTION -->
+        <div class="col-md-8">
+        <h4>Project Title</h4>
+        <p>
+        Cappuccino, java black trifecta caramelization latte cream in crema percolator. Id to go sweet, froth macchiato, arabica cortado milk whipped trifecta. Filter single origin, americano milk instant affogato crema wings. Bar id black single origin pumpkin spice sit mazagran flavour. Froth, eu milk coffee kopi-luwak fair trade skinny flavour.
+        </p>
+        </div>
+    </div>
+
+        <div class="row">
+        <!-- PROJECT IMAGE -->
+        <div class="col-md-4 center-inline">
+            <img src="images/animal-blue.png">
+        </div>
+        <!-- PROJECT DESCRIPTION -->
+        <div class="col-md-8">
+        <h4>Project Title</h4>
+        <p>
+        Cappuccino, java black trifecta caramelization latte cream in crema percolator. Id to go sweet, froth macchiato, arabica cortado milk whipped trifecta. Filter single origin, americano milk instant affogato crema wings. Bar id black single origin pumpkin spice sit mazagran flavour. Froth, eu milk coffee kopi-luwak fair trade skinny flavour.
+        </p>
+        </div>
+    </div>
+</div>
 
 @stop
