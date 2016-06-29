62443f4e00f10e2400507d830de6900ade87a90b
Rachel <rachel.loziuk@gmail.com>
2016-06-28 19:49:15 -0500

    Project-01:number of columns in banner image changes when user selects colors

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,34 @@
 $(document).ready(function() {
     
+    // COUNT THE CHECKBOXES AND MAKE EQUAL NUMBER OF COLUMNS 
+    make_columns();
+    
     // HIDE OR SHOW STUDENTS WHEN A COLOR IS CHECKED:
     show_students();  //shown by default
+
     $('#colors input').change(function() {
+        make_columns();
         show_students();
     });
 
 });
 
-    // SHOW THE STUDENTS WHOSE FAVORITE COLOR IS CHECKED:
-    function show_students() {
-        $('.student').hide();
+// COUNT NUMBER OF CHECKBOXES CHECKED AND MAKE EQUAL NUMBER OF COLUMNS IN BANNER IMAGE:
+function make_columns() {
+    $('#color-swatch').empty();
 
-        $('input:checked').each(function() {
-            var color = $(this).val();
-            $('.student.' + color ).show();
-        });
-    } 
+    var count_colors = $('#color-checkboxes input:checked').length;
+    $('#color-checkboxes input:checked').each(function(){
+        $('#color-swatch').append('<div id="color-' + count_colors + '-column" class="color-block">');
+    });
+}
+
+// SHOW THE STUDENTS WHOSE FAVORITE COLOR IS CHECKED:
+function show_students() {
+    $('.student').hide();
+
+    $('input:checked').each(function() {
+        var color = $(this).val();
+        $('.student.' + color ).show();
+    });
+} 
--- resources/views/projects/project-01.blade.php
+++ resources/views/projects/project-01.blade.php
@@ -16,11 +16,7 @@ Colors
 <!-- BODY -->
 <div id="colors">
     <!--BANNER IMAGE -->
-    <!-- <div class="row"> -->
-        <div id="color-picker" class="center-block">           
-            <div id="color-2" class="color-block"></div><div id="color-2" class="color-block"></div>
-        </div>
-    <!-- </div> -->
+        <div id="color-swatch" class="center-block"></div>
 
     <!-- INSTRUCTIONS -->
     <div class="center-inline">
@@ -28,40 +24,42 @@ Colors
     </div>
 
     <!-- COLOR CHECKBOXES: -->
-    <div class="row">
-        <div id="red" class="col-md-2 col-md-offset-2 col-xs-offset-1 display_color">
-            <input type="checkbox" checked="checked" id="red-checkbox" value="red">
-            <label for="red-checkbox">Red</label>
+    <div id="color-checkboxes">
+        <div class="row">
+            <div class="col-md-2 col-md-offset-2 col-xs-offset-1">
+                <input type="checkbox" checked="checked" id="red-checkbox" value="red">
+                <label for="red-checkbox">Red</label>
+            </div>
+            <div class="col-md-2 col-md-offset-0 col-xs-offset-1">
+                <input type="checkbox" checked="checked" id="orange-checkbox" value="orange">
+                <label for="orange-checkbox">Orange</label>
+            </div>
+            <div class="col-md-2 col-md-offset-0 col-xs-offset-1">
+                <input type="checkbox" checked="checked" id="yellow-checkbox" value="yellow">
+                <label for="yellow-checkbox">Yellow</label>
+            </div>
+            <div class="col-md-2 col-md-offset-0 col-xs-offset-1">
+                <input type="checkbox" checked="checked" id="green-checkbox" value="green">
+                <label for="green-checkbox">Green</label>
+            </div>
         </div>
-        <div class="col-md-2 col-md-offset-0 col-xs-offset-1">
-            <input type="checkbox" checked="checked" id="orange-checkbox" value="orange">
-            <label for="orange-checkbox">Orange</label>
-        </div>
-        <div class="col-md-2 col-md-offset-0 col-xs-offset-1">
-            <input type="checkbox" checked="checked" id="yellow-checkbox" value="yellow">
-            <label for="yellow-checkbox">Yellow</label>
-        </div>
-        <div class="col-md-2 col-md-offset-0 col-xs-offset-1">
-            <input type="checkbox" checked="checked" id="green-checkbox" value="green">
-            <label for="green-checkbox">Green</label>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-2 col-md-offset-2 col-xs-offset-1">
-            <input type="checkbox" checked="checked" id="blue-checkbox" value="blue">
-            <label for="blue-checkbox">Blue</label>
-        </div>
-        <div class="col-md-2 col-md-offset-0 col-xs-offset-1">
-            <input type="checkbox" checked="checked" id="purple-checkbox" value="purple">
-            <label for="purple-checkbox">Purple</label>
-        </div>
-        <div class="col-md-2 col-md-offset-0 col-xs-offset-1">
-            <input type="checkbox" checked="checked" id="black-checkbox" value="black">
-            <label for="black-checkbox">Black</label>
-        </div>
-        <div class="col-md-2 col-md-offset-0 col-xs-offset-1">
-            <input type="checkbox" checked="checked" id="white-checkbox" value="white">
-            <label for="white-checkbox">White</label>
+        <div class="row">
+            <div class="col-md-2 col-md-offset-2 col-xs-offset-1">
+                <input type="checkbox" checked="checked" id="blue-checkbox" value="blue">
+                <label for="blue-checkbox">Blue</label>
+            </div>
+            <div class="col-md-2 col-md-offset-0 col-xs-offset-1">
+                <input type="checkbox" checked="checked" id="purple-checkbox" value="purple">
+                <label for="purple-checkbox">Purple</label>
+            </div>
+            <div class="col-md-2 col-md-offset-0 col-xs-offset-1">
+                <input type="checkbox" checked="checked" id="black-checkbox" value="black">
+                <label for="black-checkbox">Black</label>
+            </div>
+            <div class="col-md-2 col-md-offset-0 col-xs-offset-1">
+                <input type="checkbox" checked="checked" id="white-checkbox" value="white">
+                <label for="white-checkbox">White</label>
+            </div>
         </div>
     </div>
 
