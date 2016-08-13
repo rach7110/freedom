a6b8b06e1399a55dd0a1f405e20734d2fffbdf2b
Rachel <rachel.loziuk@gmail.com>
2016-08-13 12:38:17 -0500

    comments and improved readablitiy in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,31 @@
 $(document).ready(function() {
-    
-    // COUNT THE CHECKBOXES AND MAKE EQUAL NUMBER OF COLUMNS 
-    make_columns();
-    
-    // HIDE OR SHOW STUDENTS WHEN A COLOR IS CHECKED:
-    show_students();  //shown by default
 
-    $('#colors input').change(function() {
+    // COUNT THE CHECKBOXES AND MAKE EQUAL NUMBER OF COLUMNS
+    make_columns();
+
+    //SHOW STUDENTS WHEN THEIR FAVORITE COLOR IS CHECKED:
+    show_students();
+
+    $('#color-checkboxes input').change(function() {
         make_columns();
         show_students();
     });
 
 });
 
-// COUNT NUMBER OF CHECKBOXES CHECKED AND MAKE EQUAL NUMBER OF COLUMNS IN BANNER IMAGE:
+// VARY COLUMN WIDTHS AND COLORS IN BANNER IMAGE:
 function make_columns() {
-    $('#color-swatch').empty();
+    $('#color-swatch').empty();  //remove existing columns first
 
+    // COUNT NUMBER OF CHECKBOXES SELECTED FOR COLUMN WIDTH
     var count_colors = $('#color-checkboxes input:checked').length;
+
     $('#color-checkboxes input:checked').each(function(){
-        var color = $(this).val();
+        // NEW COLUMN
         var column = $('<div id="color-' + count_colors + '-column" class="color-block">');
-        
         $('#color-swatch').append(column);
+        // CHECKBOX COLOR
+        var color = $(this).val();
         $(column).css('background-color', color);
 
     });
@@ -37,4 +40,4 @@ function show_students() {
         console.log(color);
         $('.student.' + color ).show();
     });
-} 
+}
