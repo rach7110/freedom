eec2a07ea17f369e048c7a1ac411695e2adaa611
Rachel <rachel.loziuk@gmail.com>
2016-09-14 17:49:56 -0500

    style: use buttons for links to project code on Github

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -24,21 +24,21 @@ Projects
         <div class="col-md-8">
           <a href="{{ route('project-01') }}" target="_blank"><h3>Favorite Colors</h3></a>
           <div class="project-description">
-            <p>A single page app that demonstates interactive styling. It uses a homemade CSS grid to vary column widths in the color swatch. It uses JQuery to assign CSS styling and property values in order to display matching colors and students. It also uses MVC architecture so users can view and add new students to the database. And it uses HTML form validation.</p>
+            <p>Demonstates interactive styling. Uses a homemade CSS grid to vary column widths in the color swatch. Uses JQuery to assign CSS styling and property values and display matching colors and students. Also uses MVC architecture so users can view and add new students to the database. And uses HTML form validation.</p>
             <!-- CODE BASE-->
             <p class="tools-header">Tools & Concepts:</p>
-            <p class="instruction">(Click to view code base)</p>
-            <ul>
-              <li><a href="https://github.com/rach7110/freedom/blob/master/public/js/script.js" target="_blank">Javascript & JQuery</a></li>
-              <li><a href="https://github.com/rach7110/freedom/blob/master/public/css/style.css" target="_blank">CSS Grid</a></li>
-              <li>MVC Framework</li>
-              <li>Database Queries</li>
-              <li><a href="https://github.com/rach7110/freedom/blob/master/resources/views/projects/project-01.blade.php" target="_blank">HTML & Form Validation</a></li>
-            </ul>
+            <div class="btn-group" role="group" aria-label="...">
+              <button type="button" class="btn btn-default"><a href="https://github.com/rach7110/freedom/blob/master/public/js/script.js" target="_blank">Javascript & JQuery</a></button>
+              <button type="button" class="btn btn-default"><a href="https://github.com/rach7110/freedom/blob/master/public/css/style.css" target="_blank">CSS Grid</a></button>
+              <button type="button" class="btn btn-default"><a href="https://github.com/rach7110/freedom/blob/master/resources/views/projects/project-01.blade.php" target="_blank">HTML & Form Validation</a></button>
+              <button type="button" class="btn btn-default nohover">MVC Architecture</button>
+              <button type="button" class="btn btn-default nohover">Database Queries</button>
+            </div>
         </div>
       </div>
     </div>
     <!-- PROJECT-02 -->
+    </br>
     <div class="row">
         <!-- PROJECT IMAGE -->
         <div class="col-md-4 center-inline">
@@ -53,16 +53,16 @@ Projects
               <p>In order to abide by EU law, a company needs to notify its users about cookies. This site uses CSS to style the notificaiton bar. It uses JQuery to animate the notification bar and to set a browser cookie when the user clicks it. If a broswer cookie is present the next time the page is visited, then the notification bar will no longer display.</p>
               <!-- CODE BASE-->
               <p class="tools-header">Tools & Concepts:</p>
-              <p class="instruction">(Click to view code base)</p>
-              <ul>
-                <li>Browser Cookies</li>
-                <li><a href="https://github.com/rach7110/freedom/blob/master/public/js/script.js" target="_blank">JQuery</a></li>
-                <li><a href="https://github.com/rach7110/freedom/blob/master/public/css/style.css" target="_blank">CSS</a></li>
-              </ul>
+              <div class="btn-group" role="group" aria-label="...">
+                <button type="button" class="btn btn-default"><a href="https://github.com/rach7110/freedom/blob/master/public/js/script.js" target="_blank">JQuery</a></button>
+                <button type="button" class="btn btn-default"><a href="https://github.com/rach7110/freedom/blob/master/public/css/style.css" target="_blank">CSS</a></button>
+                <button type="button" class="btn btn-default nohover">Browser Cookies</button>
+              </div>
             </div>
         </div>
     </div>
     <!-- PROJECT-03 -->
+    </br>
     <div class="row">
         <!-- PROJECT IMAGE -->
         <div class="col-md-4 center-inline">
