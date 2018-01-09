d46a8af513dd77b76b4c5f6df9fac3dc79a2f218
Rachel <rachel.loziuk@gmail.com>
2018-01-09 15:18:20 -0600

    create a Post migration and model

---
+++ database/migrations/2018_01_09_205823_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug')->unique()->nullable();
+            $table->string('title')->nullable();
+            $table->text('content')->nullable();
+            $table->text('publish_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
