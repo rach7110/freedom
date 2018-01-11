7ac61223e565745d9f83ed488367de64750e5e01
Rachel <rachel.loziuk@gmail.com>
2018-01-11 16:57:39 -0600

    style posts and add page for week-2

---
+++ resources/views/posts/week-2.blade.php
@@ -0,0 +1,15 @@
+@extends('posts.show')
+
+
+@section('post-content')
+<div class="blog">
+    <div class="row" id="banner">
+    <img src="/images/week-2.jpg">
+    </div>
+
+    <div class="inset-column">
+        <p>
+        Lorem ipsum.
+        </p>
+
+@stop
