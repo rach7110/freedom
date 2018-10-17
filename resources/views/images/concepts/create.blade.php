e5fb25b986b314e3f36fa85e74ef5c2c6b2fa0a9
Rachel <rachel.loziuk@gmail.com>
2018-10-17 15:39:01 -0500

    create a view to sbmit an image analysis.

---
+++ resources/views/images/concepts/create.blade.php
@@ -0,0 +1,7 @@
+<h1>Image Concepts</h1>
+<h3>This is where we will allow the user to upload image(-s)</h3>
+
+<form action="{{route('image')}}" method="post">
+    {!! csrf_field() !!}
+    <input type="submit" value="Submit">
+</form>
